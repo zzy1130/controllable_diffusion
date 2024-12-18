b83c8c3f49544036839c6d90f640c721c0bbd763
zzy1130 <zzhi20021130@gmail.com>
2024-12-18 18:08:10 +0800

    add condition choice in `lora_generate`

--- lora_generate.py
+++ lora_generate.py
@@ -1,7 +1,7 @@
 import torch
 from diffusers import DiffusionPipeline, LCMScheduler, ControlNetModel
 import matplotlib.pyplot as plt
-from controlnet_aux import OpenposeDetector, HEDdetector
+from controlnet_aux import OpenposeDetector, HEDdetector, PidiNetDetector
 from PIL import Image
 from transformers import pipeline
 import numpy as np
@@ -53,9 +53,11 @@ import argparse
 # images.save('lora_output.png')
 parser = argparse.ArgumentParser(description="Generate images using Controlnet")
 parser.add_argument(
-    "--image_path", type=str, default='', help="Condition choice"
+    "--image_path", type=str, default='/userhome/30/zyzhong2/controllable_diffussion/images/IMG_47CF507AEBD7-1.png', help="Condition choice"
 )
-
+parser.add_argument(
+    "--condition", type=str, default='soft_edge', help="Condition choice"
+)
 class ImageGenerator:
     def __init__(self, model_name="stabilityai/stable-diffusion-xl-base-1.0", device="cuda"):
         # Initialize the diffusion pipeline
@@ -88,18 +90,26 @@ class ImageGenerator:
             control_image = np.concatenate([control_image, control_image, control_image], axis=2)
             control_image = Image.fromarray(control_image)
             return control_image
-
+        elif method == 'soft_edge':
+            image = Image.open('/userhome/30/zyzhong2/controllable_diffusion/images/beauty.png')
+            processor = HEDdetector.from_pretrained('lllyasviel/Annotators')
+            processor = PidiNetDetector.from_pretrained('lllyasviel/Annotators')
+            control_image = processor(image, safe=True)
+            control_image.save("/userhome/30/zyzhong2/controllable_diffusion/control_image/control_hed.png")
+            return control_image
         # Additional methods (e.g., 'pose', 'scribble') can be added here as needed
 
-    def generate_image(self, prompt, control_image):
+    def generate_image(self, prompt, control_image, method='soft_edge'):
         # Load the ControlNet model
-        controlnet = ControlNetModel.from_pretrained(
-            "lllyasviel/sd-controlnet-depth", torch_dtype=torch.float16
-        )
-
+        if method == 'soft_edge':
+            checkpoint = "lllyasviel/control_v11p_sd15_softedge"
+            controlnet = ControlNetModel.from_pretrained(checkpoint, torch_dtype=torch.float16)
+        else:
+            controlnet = ControlNetModel.from_pretrained(
+                "lllyasviel/sd-controlnet-depth", torch_dtype=torch.float16
+            )
         # Combine LoRAs
         self.pipe.set_adapters(["lcm", "Chinese Ink"], adapter_weights=[1.0, 0.8])
-
         # Generate the image
         generator = torch.manual_seed(1)
         negative_prompt = "monochrome, lowres, bad anatomy, worst quality, low quality, ugly"
@@ -110,6 +120,7 @@ class ImageGenerator:
 # Example usage
 if __name__ == "__main__":
     args = parser.parse_args()
+    method = args.condition
     image_path = args.image_path
     # image_path = "/userhome/30/zyzhong2/controllable_diffussion/images/IMG_47CF507AEBD7-1.png"
     interrogator = ImageInterrogator()
@@ -119,6 +130,6 @@ if __name__ == "__main__":
     print(prompt)
     # prompt = "Chinese Ink, A cat with a box and a knife in his hand, white and orange breastplate, wearing torn clothes, full body mascot, inspired by Dong Kingman, bassist, brown pants, artist rendition, aliased, artistic rendition, instruments, block head, trading card, by Noami, a character portrait, 8k"
     generator = ImageGenerator()
-    control_image = generator.load_control_image(image_path, method='depth')
-    output_image = generator.generate_image(prompt, control_image)
+    control_image = generator.load_control_image(image_path, method=method)
+    output_image = generator.generate_image(prompt, control_image, method=method)
     output_image.save('/userhome/30/zyzhong2/controllable_diffussion/out/lora_output.png')
